0517a6f13276d65b09d82a53aedab00223aa5165
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-04-01 17:12:31 -0400

    Initial commit

---
+++ src/main/java/com/czellmer1324/licenseplategame/LicensePlateGameApplication.java
@@ -0,0 +1,13 @@
+package com.czellmer1324.licenseplategame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensePlateGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensePlateGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czellmer1324/licenseplategame/LicensePlateGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czellmer1324.licenseplategame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensePlateGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
