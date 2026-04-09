e5cb4313fc01ddf28c00ad353835f57a6c0a6bfc
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-04-09 18:38:49 -0400

    More cors changes

--- src/main/java/com/czellmer1324/licenseplategame/config/SecurityConfig.java
+++ src/main/java/com/czellmer1324/licenseplategame/config/SecurityConfig.java
@@ -48,10 +48,10 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration  configuration = new CorsConfiguration();
+        CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("https://license-plate-game.onrender.com/"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedOrigins(List.of("https://license-plate-game.onrender.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
         configuration.setAllowCredentials(true);
--- src/main/java/com/czellmer1324/licenseplategame/jwt/AuthTokenFilter.java
+++ src/main/java/com/czellmer1324/licenseplategame/jwt/AuthTokenFilter.java
@@ -30,7 +30,13 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        if ("OPTIONS".equals(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        
         try {
+
             // get the token from the header
             String token = utils.getTokenFromHeader(request);
 
