1264fd6602d5a2000534cd61e017ffe167d2cc91
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-04-13 20:39:58 -0400

    Change to cors, use frontend url from .props

--- src/main/java/com/czellmer1324/licenseplategame/config/SecurityConfig.java
+++ src/main/java/com/czellmer1324/licenseplategame/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.czellmer1324.licenseplategame.config;
 
 import com.czellmer1324.licenseplategame.jwt.AuthTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -45,11 +46,14 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Value("${frontend.url}")
+    private String url;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:63342"));
+        configuration.setAllowedOrigins(List.of(url));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
