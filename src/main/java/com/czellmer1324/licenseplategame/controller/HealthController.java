1997713484e08bf7a2ee7c8a7283b5d52558d557
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-04-21 18:30:12 -0400

    Added health endpoint

---
+++ src/main/java/com/czellmer1324/licenseplategame/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.czellmer1324.licenseplategame.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/health")
+@CrossOrigin(origins = "https://cronjob.com")
+public class HealthController {
+    @GetMapping()
+    public ResponseEntity<?> getHealth() {
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Message", "Still running"));
+    }
+}
